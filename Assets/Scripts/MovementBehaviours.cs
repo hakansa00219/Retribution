23579d57a074e52cc53f1b61941452db69155c6b
Hakan Sarıoğlu <61359973+hakansa00219@users.noreply.github.com>
2025-02-19 23:27:50 +0300

    after gamejam enemy movement development. developing some movement behaviours

--- Assets/Scripts/MovementBehaviours.cs
+++ Assets/Scripts/MovementBehaviours.cs
@@ -1,13 +1,45 @@
 using System;
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Scriptable Objects/MovementBehaviours", fileName = "MovementBehaviours")]
 public class MovementBehaviours : SerializedScriptableObject
 {
-    public Dictionary<string, Func<float, float>> Behaviours = new Dictionary<string, Func<float, float>>()
+    public Dictionary<string, Func<Enemy, UniTaskVoid>> Behaviours = new Dictionary<string, Func<Enemy, UniTaskVoid>>()
     {
-        {"LeftRightMovement", (x) => x * 10 },
+        {"LeftRightMovement", LeftRightMovement },
     };
+
+    //need to be canceled when camera changed
+    private static async UniTaskVoid LeftRightMovement(Enemy e)
+    {
+        int rng = UnityEngine.Random.Range(0, 2);
+        switch (rng)
+        {
+            //go +x
+            case 0:
+                while (e != null && e.gameObject != null && e.gameObject.activeInHierarchy)
+                {
+                    await MovementHelper.MoveTransformAsync(e.transform,
+                        new Vector3(4f, e.transform.position.y, e.transform.position.z), 1f);
+                    await MovementHelper.MoveTransformAsync(e.transform,
+                        new Vector3(-4f, e.transform.position.y, e.transform.position.z), 2f);
+                } 
+                break;
+            //go -x
+            case 1:
+                while (e != null && e.gameObject != null && e.gameObject.activeInHierarchy)
+                {
+                    await MovementHelper.MoveTransformAsync(e.transform,
+                        new Vector3(-4f, e.transform.position.y, e.transform.position.z), 1f);
+                    await MovementHelper.MoveTransformAsync(e.transform,
+                        new Vector3(4f, e.transform.position.y, e.transform.position.z), 2f);
+                }
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
 }
